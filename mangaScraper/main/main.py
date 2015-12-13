b408dff5454a555383a74c13da7896293eb45cdd
cojoco <cojoco@users.noreply.github.com>
2015-12-12 22:28:53 -0700

    No longer need to enter how many pages

--- mangaScraper/main/main.py
+++ mangaScraper/main/main.py
@@ -1,6 +1,9 @@
 import requests
 import shutil
 from bs4 import BeautifulSoup
+from zipfile import ZipFile
+import glob
+import os
 
 def pad(number):
     if number < 10:
@@ -8,20 +11,39 @@ def pad(number):
     else:
         return str(number)
 
-urls = ['http://www.mangatown.com/manga/death_note/v00/c001/']
-
-for i in xrange(2,53):
-    urls.append('http://www.mangatown.com/manga/death_note/v00/c001/' + str(i) + '.html')
+firstPage = str(raw_input("Please enter the URL of the first page (including trailing slash): "))
 
 images = []
 
-for url in urls:
+url = firstPage
+
+pageNumber = 2
+
+while True:
     resp = requests.get(url)
+    if (resp.status_code == 404):
+        break
     soup = BeautifulSoup(resp.text, 'lxml')
     image = soup.find(id="image")["src"]
     images.append(image)
+    url = firstPage + str(pageNumber) + '.html'
+    pageNumber += 1
+
+print str(pageNumber-2) + " pages found."
 
 for i,image in enumerate(images):
-    with open("images/file" + pad(i+1) + ".jpg", 'w') as file:
+    with open("/home/connor/Documents/comic/" + pad(i+1) + ".jpg", 'w') as file:
         page = requests.get(image, stream=True)
         shutil.copyfileobj(page.raw, file)
+
+print "Image files written"
+
+with ZipFile('/home/connor/Documents/deathnote.cbz', 'w') as myZip:
+    for filename in sorted(glob.iglob('/home/connor/Documents/comic/*')):
+        myZip.write(filename)
+        os.remove(filename)
+    myZip.close()
+
+print "Zip file written."
+
+print "Scrape complete."
